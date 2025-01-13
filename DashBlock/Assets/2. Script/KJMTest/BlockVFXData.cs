c0109fbae921b33822e8488f8106c91b48107484
zzangmin <wjdals1212gh@gmail.com>
2025-01-13 18:01:39 +0900

    [Add] Atlas Packer - 2
    TODO : 씬 전환

---
+++ DashBlock/Assets/2. Script/KJMTest/BlockVFXData.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockVFXData : MonoBehaviour
+{
+    public static readonly Vector2[] blockUVs = new Vector2[4]
+    {
+        new Vector2(0.0f, 0.0f),
+        new Vector2(0.0f, 1.0f),
+        new Vector2(1.0f, 1.0f),
+        new Vector2(1.0f, 0.0f)
+
+    };
+
+}
